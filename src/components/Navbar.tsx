6898d80a4962cf301f42eecccb0709a806259fdc
Olawoyin <yustee2017@gmail.com>
2025-07-03 10:44:09 +0100

    navbar icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import  { useState } from "react";
-import { FiMenu } from "react-icons/fi";
+// import { FiMenu } from "react-icons/fi";
 import { FaFacebookF, FaYoutube } from "react-icons/fa";
 import { GoSearch } from "react-icons/go";
 import { IoIosHeartEmpty } from "react-icons/io";
@@ -7,34 +7,32 @@ import { LuUserRound } from "react-icons/lu";
 import { RiInstagramFill } from "react-icons/ri";
 import { IoCartOutline } from "react-icons/io5";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router-dom";
 
 const HeaderBar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="w-full bg-white s sticky border-b top-0 z-50">
+    <header className="w-full bg-white s sticky border-b border-gray-600 top-0 z-50">
       <div className="mx-auto px-4 md:px-10 py-5  flex items-center justify-between">
         {/* Mobile: Menu Icon */}
         <div className="flex items-center gap-4 md:hidden">
-          <button
-            onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-            className="text-black text-xl"
-          >
-            <FiMenu />
-          </button>
+          
+            <RxHamburgerMenu onClick={() => setMobileMenuOpen(!isMobileMenuOpen)} size={30}/>
+          
         </div>
 
         {/* Desktop: Social Icons */}
         <div className="hidden md:flex items-center gap-4 text-gray-600 text-sm">
-          <a href="#" aria-label="Facebook"><FaFacebookF /></a>
-          <a href="#" aria-label="Instagram"><RiInstagramFill /></a>
-          <a href="#" aria-label="Whatsapp"><IoLogoWhatsapp /></a>
+          <a href="#" aria-label="Facebook"><FaFacebookF  /></a>
+          <a href="#" aria-label="Instagram"><RiInstagramFill  /></a>
+          <a href="#" aria-label="Whatsapp"><IoLogoWhatsapp  /></a>
           <a href="#" aria-label="YouTube"><FaYoutube /></a>
         </div>
 
         {/* Logo (Center) */}
-        <div className="flex-1 flex justify-center">
+        <div className="flex justify-center">
             <Link to="/shop">
                 <h3  className="tracking-[8px] text-2xl font-extrabold">ANEWENA</h3>
             </Link>
@@ -43,19 +41,19 @@ const HeaderBar = () => {
         {/* Icons Right (Desktop: All, Mobile: One icon) */}
         <div className="flex items-center gap-4 text-black">
           <div className="hidden md:flex items-center gap-4">
-           <a href="/my-account" aria-label="Account" className="text-lg">
+           <a href="/my-account" aria-label="Account" className="text-xl">
               <LuUserRound />
             </a>
-          <a href="/cart" aria-label="Cart" className="text-lg">
+          <a href="/cart" aria-label="Cart" className="text-xl">
             <GoSearch />
           </a>
-          <a href="/cart" aria-label="Cart" className="text-lg">
+          <a href="/cart" aria-label="Cart" className="text-xl">
             <IoIosHeartEmpty />
           </a>
           </div>
           
-            <a href="/cart" aria-label="Cart" className="text-lg">
-            <IoCartOutline />
+            <a href="/cart" aria-label="Cart" className="text-3xl md:text-xl">
+            <IoCartOutline  />
           </a>
         </div>
       </div>
